022bc3257499308dff32a0f6ee4d8646f349e06b
fahadanwar11 <fahadanwar363@gmail.com>
2025-03-01 03:33:58 +0500

    code

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,14 +1,14 @@
-import bannerImg from "./bannerImg.jpeg";
-import AboutUsBanner from "./AboutUsBanner.jpeg";
-import AboutUsCameraOne from "./AboutUsCamera1.jpg";
-import AboutUsCameraTwo from "./AboutUsCamera2.jpg";
-import ServicesImg from "./servicesImg.jpg";
+import HOMEPAGE_BANNER_IMG from "./bannerImg.jpeg";
+import ABOUT_US_BANNER_IMG from "./AboutUsBanner.jpeg";
+import ABOUT_US_CAMERA_ONE from "./AboutUsCamera1.jpg";
+import ABOUT_US_CAMERA_TWO from "./AboutUsCamera2.jpg";
+import SERVICES_IMG from "./servicesImg.jpg";
 const IMAGES = {
-  HOMEPAGE_BANNER_IMG: bannerImg,
-  ABOUT_US_BANNER_IMG: AboutUsBanner,
-  ABOUT_US_CAMERA_ONE: AboutUsCameraOne,
-  ABOUT_US_CAMERA_TWO: AboutUsCameraTwo,
-  SERVICES_IMG: ServicesImg,
+  HOMEPAGE_BANNER_IMG,
+  ABOUT_US_BANNER_IMG,
+  ABOUT_US_CAMERA_ONE,
+  ABOUT_US_CAMERA_TWO,
+  SERVICES_IMG,
 };
 
 export default IMAGES;
