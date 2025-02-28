062eecaff1ade24df97fd206843d295ef5e9a9c5
fahadanwar11 <fahadanwar363@gmail.com>
2025-03-01 03:43:35 +0500

    code

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,11 +1,11 @@
-// import HOMEPAGE_BANNER_IMG from "./bannerImg.jpeg";
-// import ABOUT_US_BANNER_IMG from "./AboutUsBanner.jpeg";
+import HOMEPAGE_BANNER_IMG from "@/assets/images/bannerImg.jpeg";
+import ABOUT_US_BANNER_IMG from "@assets/images/AboutUsBanner.jpeg";
 import ABOUT_US_CAMERA_ONE from "./AboutUsCamera1.jpg";
 import ABOUT_US_CAMERA_TWO from "./AboutUsCamera2.jpg";
 import SERVICES_IMG from "./servicesImg.jpg";
 const IMAGES = {
-  // HOMEPAGE_BANNER_IMG,
-  // ABOUT_US_BANNER_IMG,
+  HOMEPAGE_BANNER_IMG,
+  ABOUT_US_BANNER_IMG,
   ABOUT_US_CAMERA_ONE,
   ABOUT_US_CAMERA_TWO,
   SERVICES_IMG,
